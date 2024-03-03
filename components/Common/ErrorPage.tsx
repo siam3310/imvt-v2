9b076818ac55a6c8d11fb477b380abd20acec5ae
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-03-04 01:36:25 +0530

    Fix formatting in ErrorPage component

--- components/Common/ErrorPage.tsx
+++ components/Common/ErrorPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react"
+import Link from "next/link";
 
 const ErrorPage = () => {
     return (
@@ -9,7 +9,7 @@ const ErrorPage = () => {
                     <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
                         <span className="sr-only">Error</span>404
                     </h2>
-                    <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+                    <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn&apos;t find this page.</p>
                     <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
                     <Link rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded dark:bg-white dark:text-gray-900">Back to homepage</Link>
                 </div>
